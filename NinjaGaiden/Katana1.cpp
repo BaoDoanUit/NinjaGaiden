f2c0ba8e3ba8b33fd67bb8f3d5fa934fd87b1e18
BaoDoanUit <baodoanuit@gmail.com>
2019-06-14 14:54:26 +0700

    commit katana

---
+++ NinjaGaiden/Katana1.cpp
@@ -0,0 +1,34 @@
+﻿#include "Katana1.h"
+
+Katana1::Katana1()
+{
+	TypeWeapon = eType::KATANA1;
+	GTObject = TextureManager::GetInstance()->GetTexture(eType::KATANA1);
+	GSObject = new Sprite(GTObject, 70);
+	dx = 0;
+}
+
+Katana1::~Katana1()
+{
+}
+
+void Katana1::Update(Camera *camera, int t)
+{
+	
+	IsFinish = GSObject->GetIndex() == 3;
+	int StartFrame = 0;  
+	int EndFrame = 3;
+
+	if (StartFrame <= GSObject->GetIndex() && GSObject->GetIndex() < EndFrame)
+		GSObject->Update(t);
+	else
+	{
+		GSObject->SelectIndex(StartFrame);
+	}
+}
+
+void Katana1::Create(float ninjaGaiden_x, float ninjaGaiden_y, int sm_trend)
+{
+	Weapon::Create(ninjaGaiden_x - 5, ninjaGaiden_y + 45, sm_trend);
+	Vx = KATANA_VX*Trend;
+}
---
+++ NinjaGaiden/Katana1.h
@@ -0,0 +1,16 @@
+﻿#ifndef _SWORD_H_
+#define _SWORD_H_
+#include "Weapon.h"
+#include "TextureManager.h"
+#define KATANA_VX 0.33f
+
+class Katana1 : public Weapon
+{
+public:
+	Katana1();
+	~Katana1();
+	int dx;//Khoảng cách để kết thúc dao không cho vẽ
+	virtual void Update(Camera *camera, int t);
+	virtual void Create(float ninjaGaiden_x, float ninjaGaiden_y, int ninjaGaiden_trend);
+};
+#endif
